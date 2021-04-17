30e12987ae2823c5544692bab644bcac4b8f2f04
benjaguti <benjagcj@gmail.com>
2021-04-16 21:12:31 -0300

    TP1 Ejercicio POO labs

---
+++ EjercicioPoo/EjercicioPoo/Entidades/Transporte.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPoo.Entidades
+{
+    public abstract class Transporte
+    {
+        private int pasajeros;
+        private string nombre;
+        private string tipoTransporte;
+
+        public Transporte(string tipoTransporte,string nombre, int cantPasajeros) 
+        {
+            this.tipoTransporte = tipoTransporte;
+            this.nombre = nombre; 
+            this.pasajeros = cantPasajeros;
+        }
+        public string TipoTransporte
+        {
+            get => tipoTransporte;
+            set => tipoTransporte = value;
+        }
+        public string Nombre 
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public int Pasajeros
+        {
+            get => pasajeros;
+            set => pasajeros = value;
+
+        }
+
+        public abstract string Avanzar();
+
+        public abstract string Detenerse();
+
+    }
+}
---
+++ EjercicioPoo/EjercicioPoo/Form1.cs
@@ -0,0 +1,88 @@
+﻿using EjercicioPoo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioPoo
+{
+    public partial class FrmVisualizaciónTrans : Form
+    {
+        public FrmVisualizaciónTrans()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        List<Transporte> transporte = new List<Transporte>
+            {
+                new Avion("Avion","Boeing 747", 20),
+                new Avion("Avion","Airbus A350",25),
+                new Avion("Avion","Airbus A380",24),
+                new Avion("Avion","Boeing 737",29),
+                new Avion("Avion","Boeing 747",45),
+                new Automovil("Auto","Mercedez benz", 3),
+                new Automovil("Auto","Audi",2),
+                new Automovil("Auto","Ford",3),
+                new Automovil("Auto","Fiat",4),
+                new Automovil("Auto","Renault",1)
+            };
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvTransporte.DataSource = transporte;
+        }
+
+        private void btnAviones_Click(object sender, EventArgs e)
+        {
+            List<Transporte> aviones = new List<Transporte>();
+
+            foreach (var item in transporte)
+            {
+                if (item.TipoTransporte.Equals("Avion"))
+                {
+                    aviones.Add(item);
+                }
+            }
+
+            dgvTransporte.DataSource = aviones;
+
+        }
+
+        private void btnMostrarAutos_Click(object sender, EventArgs e)
+        {
+            List<Transporte> autos = new List<Transporte>();
+
+            foreach (var item in transporte)
+            {
+                if (item.TipoTransporte.Equals("Auto"))
+                {
+                    autos.Add(item);
+                }
+            }
+
+            dgvTransporte.DataSource = autos;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BringToFront();
+        }
+    }
+}
